67a41322a1556e482605fc1414710d0f7b21a1d2
박동건 <hbd9425@gmail.com>
2020-05-12 19:01:40 +0900

    add: middleware

--- back/index.js
+++ back/index.js
@@ -1,15 +1,43 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
+
 const db = require("./models");
+const userAPIRouter = require("./routes/user");
+const postAPIRouter = require("./routes/post");
+const postsAPIRouter = require("./routes/posts");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express();
 db.sequelize.sync();
-const port = 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello, express");
-});
-app.get("/about", (req, res) => {
-  res.send("Hello, about!");
-});
+// 아래 두 줄은 json형식의 본문, form을 처리하는 것
+app.use(express.json()); // express에서 지원해줘서 bodyparser필요없음!
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(cors());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true, // 자바스크립트로 접근하는 것 제한
+      secure: false, // https를 쓸 때 true로 바꾸면 됨
+    },
+  })
+);
+
+app.use("/api/user", userAPIRouter);
+app.use("/api/post", postAPIRouter);
+app.use("/api/posts", postsAPIRouter);
+
+const port = 8080;
 app.listen(port, () => {
   console.log(`server is running on http://localhot:${port}`);
 });
