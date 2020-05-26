6b9096dad4181f844979adfa6be62f4902234c76
박동건 <hbd9425@gmail.com>
2020-05-26 18:53:29 +0900

    images test data

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -1,34 +1,68 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { Icon } from "antd";
+import ImagesZoom from "./imagesZoom";
 
 const PostImages = ({ images }) => {
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+  const onClose = useCallback(() => {
+    setShowImagesZoom(false);
+  }, []);
+
   if (images.length === 1) {
-    return <img src={`http://localhost:8080/${images[0].src}`} />;
+    return (
+      <>
+        <img src={`http://localhost:8080/${images[0].src}`} onClick={onZoom} />
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+      </>
+    );
   }
   if (images.length === 2) {
     return (
-      <div>
-        <img src={`http://localhost:8080/${images[0].src}`} width="50%" />
-        <img src={`http://localhost:8080/${images[1].src}`} width="50%" />
-      </div>
+      <>
+        <div>
+          <img
+            src={`http://localhost:8080/${images[0].src}`}
+            width="50%"
+            onClick={onZoom}
+          />
+          <img
+            src={`http://localhost:8080/${images[1].src}`}
+            width="50%"
+            onClick={onZoom}
+          />
+          {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+        </div>
+      </>
     );
   }
   return (
-    <div>
-      <img src={`http://localhost:8080/${images[0].src}`} width="50%" />
-      <div
-        style={{
-          display: "inline-block",
-          width: "50%",
-          textAlign: "center",
-          verticalAlign: "middle",
-        }}
-      >
-        <Icon type="plus" />
-        <br />
-        {images.length - 1}개의 사진 더보기
+    <>
+      <div>
+        <img
+          src={`http://localhost:8080/${images[0].src}`}
+          width="50%"
+          onClick={onZoom}
+        />
+        <div
+          style={{
+            display: "inline-block",
+            width: "50%",
+            textAlign: "center",
+            verticalAlign: "middle",
+          }}
+          onClick={onZoom}
+        >
+          <Icon type="plus" />
+          <br />
+          {images.length - 1}
+          개의 사진 더보기
+        </div>
       </div>
-    </div>
+      {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+    </>
   );
 };
 
--- front/pages/_app.js
+++ front/pages/_app.js
@@ -19,6 +19,17 @@ const NodeBird = ({ Component, store, pageProps }) => {
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css"
           />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            charset="UTF-8"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+          />
         </Head>
         <AppLayout>
           <Component {...pageProps} />
