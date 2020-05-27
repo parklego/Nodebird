a12770a185e9a902bb94ce7d608bfc14c108e89e
박동건 <hbd9425@gmail.com>
2020-05-27 18:02:51 +0900

    add : like && unLike API

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -3,7 +3,12 @@ import Link from "next/link";
 import { Avatar, Button, Card, Comment, Form, Icon, Input, List } from "antd";
 import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
-import { ADD_COMMENT_REQUEST, LOAD_COMMENTS_REQUEST } from "../reducers/post";
+import {
+  ADD_COMMENT_REQUEST,
+  LOAD_COMMENTS_REQUEST,
+  UNLIKE_POST_REQUEST,
+  LIKE_POST_REQUEST,
+} from "../reducers/post";
 import PostImages from "../components/PostImages";
 
 const PostCard = ({ post }) => {
@@ -12,6 +17,8 @@ const PostCard = ({ post }) => {
   const { me } = useSelector((state) => state.user);
   const { commentAdded, isAddingComment } = useSelector((state) => state.post);
   const dispatch = useDispatch();
+
+  const liked = me && post.Likers && post.Likers.find((v) => v.id === me.id);
 
   const onToggleComment = useCallback(() => {
     setCommentFormOpened((prev) => !prev);
@@ -48,6 +55,23 @@ const PostCard = ({ post }) => {
     setCommentText(e.target.value);
   }, []);
 
+  const onToggleLike = useCallback(() => {
+    if (!me) {
+      return alert("로그인이 필요합니다.");
+    }
+    if (liked) {
+      // 좋아요 누른 상태
+      return dispatch({
+        type: UNLIKE_POST_REQUEST,
+        data: post.id,
+      });
+    } else {
+      dispatch({
+        type: LIKE_POST_REQUEST,
+        data: post.id,
+      });
+    }
+  }, [me && me.id, post && post.id, liked]);
   return (
     <div>
       <Card
@@ -55,7 +79,13 @@ const PostCard = ({ post }) => {
         cover={post.Images[0] && <PostImages images={post.Images} />}
         actions={[
           <Icon type="retweet" key="retweet" />,
-          <Icon type="heart" key="heart" />,
+          <Icon
+            type="heart"
+            key="heart"
+            onClick={onToggleLike}
+            theme={liked ? "twoTone" : "outlined"}
+            twoToneColor="#eb2f96"
+          />,
           <Icon type="message" key="message" onClick={onToggleComment} />,
           <Icon type="ellipsis" key="ellipsis" />,
         ]}
