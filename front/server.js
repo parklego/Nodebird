b0ca4df6664543821ca25d06e6932ae81239e6e0
박동건 <hbd9425@gmail.com>
2020-06-29 18:07:54 +0900

    fix: port

--- front/server.js
+++ front/server.js
@@ -4,7 +4,6 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const expressSession = require("express-session");
 const dotenv = require("dotenv");
-const path = require("path");
 
 const dev = process.env.NODE_ENV !== "production";
 const prod = process.env.NODE_ENV === "production";
@@ -15,8 +14,8 @@ dotenv.config();
 
 app.prepare().then(() => {
   const server = express();
+
   server.use(morgan("dev"));
-  server.use("/", express.static(path.join(__dirname, "public")));
   server.use(express.json());
   server.use(express.urlencoded({ extended: true }));
   server.use(cookieParser(process.env.COOKIE_SECRET));
@@ -31,6 +30,7 @@ app.prepare().then(() => {
       },
     })
   );
+
   server.get("/post/:id", (req, res) => {
     return app.render(req, res, "/post", { id: req.params.id });
   });
@@ -38,13 +38,16 @@ app.prepare().then(() => {
   server.get("/hashtag/:tag", (req, res) => {
     return app.render(req, res, "/hashtag", { tag: req.params.tag });
   });
+
   server.get("/user/:id", (req, res) => {
     return app.render(req, res, "/user", { id: req.params.id });
   });
+
   server.get("*", (req, res) => {
     return handle(req, res);
   });
-  server.listen(prod ? process.env.PORT : 3060, () => {
-    console.log(`next+express running on port ${process.env.PORT}`);
+
+  server.listen(3060, () => {
+    console.log("next+express running on port 3060");
   });
 });
