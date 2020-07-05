e9ead2906b0bbf20fd330fbca73e07e658119afb
박동건 <hbd9425@gmail.com>
2020-07-05 18:33:28 +0900

    add: lambda resizeing

--- lambda/index.js
+++ lambda/index.js
@@ -10,5 +10,31 @@ exports.handler = async (event, context, callback) => {
   const Key = event.Records[0].s3.object.key;
   const filename = Key.split("/")[Key.split("/").length - 1];
   const ext = Key.split(".")[Key.split(".").length - 1];
-  console.log(Key, filename, ext);
+  console.log(Bucket, Key, filename, ext);
+  const requireFormat = ext === "jpg" ? "jpeg" : ext;
+
+  try {
+    const s3Object = await S3.getObject({
+      Bucket,
+      Key,
+    }).promise();
+    console.log("original", s3Object.Body.length);
+
+    const resizedImage = await Sharp(s3Object.Body)
+      .resize(800, 800, {
+        fit: "inside",
+      })
+      .toFormat(requireFormat)
+      .toBuffer();
+
+    await S3.putObject({
+      Bucket,
+      Key: `thumb/${filename}`,
+      Body: resizedImage,
+    }).promise();
+    return callback(null, `thumb/${filename}`);
+  } catch (e) {
+    console.error(e);
+    return callback(e);
+  }
 };
